aff339d90548db63bea66ba7b424c5942e78264c
James Nimmo <james@nimmo.net.nz>
2025-02-26 21:31:10 +0000

    Migrate API keys to Iron Session

--- app/actions/api-key.ts
+++ app/actions/api-key.ts
@@ -1,50 +1,68 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import {
+  experimental_taintObjectReference,
+  experimental_taintUniqueValue,
+} from "react";
 
-interface ApiSettings {
+const IRONSESSION_SECRET = process.env.IRONSESSION_SECRET;
+
+type ApiSettings = {
   key: string | undefined;
   secret: string | undefined;
-}
+};
 
 export async function setApiKey(apiKey: string) {
-  const cookieStore = await cookies();
-  cookieStore.set("shadowserver-api-key", apiKey, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 400 * 24 * 60 * 60, // 400 days
-  });
+  const session = await getApiSettings();
+  session.key = apiKey;
+  await session.save();
 }
 
 export async function getApiSettings() {
-  const cookieStore = await cookies();
-  const settings: ApiSettings = {
-    key: cookieStore.get("shadowserver-api-key")?.value,
-    secret: cookieStore.get("shadowserver-api-secret")?.value,
-  };
+  if (IRONSESSION_SECRET === undefined) {
+    throw new Error("IRONSESSION_SECRET is not defined");
+  }
 
-  return settings;
+  const session = await getIronSession<ApiSettings>(await cookies(), {
+    password: IRONSESSION_SECRET,
+    cookieName: "apikey",
+    ttl: 0, //indefinite
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    },
+  });
+
+  experimental_taintObjectReference(
+    "Do not pass unencrypted data to the client",
+    session
+  );
+
+  return session;
 }
 
 export async function getApiKey() {
-  const cookieStore = await cookies();
-  return cookieStore.get("shadowserver-api-key")?.value;
+  const session = await getApiSettings();
+  return session.key;
 }
 
-export async function setApiSecret(apiKey: string) {
-  const cookieStore = await cookies();
-
-  cookieStore.set("shadowserver-api-secret", apiKey, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 400 * 24 * 60 * 60, // 30 days
-  });
+export async function setApiSecret(apiSecret: string) {
+  const session = await getApiSettings();
+  session.secret = apiSecret;
+  await session.save();
 }
 
 export async function getApiSecret() {
-  const cookieStore = await cookies();
+  const session = await getApiSettings();
+  if (session.secret) {
+    experimental_taintUniqueValue(
+      "Do not pass tokens to the client",
+      session,
+      session.secret
+    );
+  }
 
-  return cookieStore.get("shadowserver-api-secret")?.value;
+  return session.secret;
 }
--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "http://localhost",
-        "verbose-yodel-4xrv557jf9gj-3000.app.github.dev",
-      ],
-    },
+    taint: true,
+    // serverActions: {
+    //   allowedOrigins: [
+    //     "http://localhost",
+    //     "verbose-yodel-4xrv557jf9gj-3000.app.github.dev",
+    //   ],
+    // },
   },
 };
 
